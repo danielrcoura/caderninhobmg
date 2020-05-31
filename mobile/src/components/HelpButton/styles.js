26585a3e176cc15bb194013e94f28c83867704ea
Henrique Cotta <sndmnn@outlook.com>
2020-05-31 05:48:44 -0300

    Added help button component

---
+++ mobile/src/components/HelpButton/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.View`
+  position: absolute;
+  z-index: 10;
+  right: 10px;
+
+  align-items: center;
+  justify-content: center;
+  width: 80px;
+  height: 80px;
+  border-radius: 40px;
+  background-color: #fff;
+`;
