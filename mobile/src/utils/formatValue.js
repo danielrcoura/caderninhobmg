3c9c71f99ae2fdfbc141d88b4deb2d0bac2b6b3a
Henrique Cotta <sndmnn@outlook.com>
2020-05-31 15:57:01 -0300

    Added value formatter

--- mobile/src/utils/formatValue.js
+++ mobile/src/utils/formatValue.js
@@ -1,5 +1,25 @@
 const formatValue = (value) => {
-  return 'R$ ' + value + ',00';
-}
+  let strValue = value + '';
+  let decimalCounter = 2;
 
+  if(strValue.includes('.')) {
+    strValue = strValue.replace('.', ',');
+    let pointFound = false;
+    for(let char of strValue.split('')) {
+      if(pointFound) {
+        decimalCounter -= 1;
+      }
+      if(char === ',') {
+        pointFound = true;
+      }
+    }
+  }
+  else{
+    strValue += ',';
+  }
+  for(let cont = 0; cont < decimalCounter; cont += 1) {
+    strValue += '0';
+  }
+  return 'R$ ' + strValue;
+}
 export default formatValue;
